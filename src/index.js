617946aca03a0da6f01c583b56df838a8fae3315
Alan haikal <alanhaikal@gmail.com>
2022-03-05 14:35:26 +0700

    svg to g

--- src/index.js
+++ src/index.js
@@ -12,60 +12,6 @@ import Svg, {
 import genMatrix from './genMatrix'
 import transformMatrixIntoPath from './transformMatrixIntoPath'
 
-const renderLogo = ({
-  size,
-  logo,
-  logoSize,
-  logoBackgroundColor,
-  logoMargin,
-  logoBorderRadius
-}) => {
-  const logoPosition = (size - logoSize - logoMargin * 2) / 2
-  const logoBackgroundSize = logoSize + logoMargin * 2
-  const logoBackgroundBorderRadius =
-    logoBorderRadius + (logoMargin / logoSize) * logoBorderRadius
-
-  return (
-    <G x={logoPosition} y={logoPosition}>
-      <Defs>
-        <ClipPath id='clip-logo-background'>
-          <Rect
-            width={logoBackgroundSize}
-            height={logoBackgroundSize}
-            rx={logoBackgroundBorderRadius}
-            ry={logoBackgroundBorderRadius}
-          />
-        </ClipPath>
-        <ClipPath id='clip-logo'>
-          <Rect
-            width={logoSize}
-            height={logoSize}
-            rx={logoBorderRadius}
-            ry={logoBorderRadius}
-          />
-        </ClipPath>
-      </Defs>
-      <G>
-        <Rect
-          width={logoBackgroundSize}
-          height={logoBackgroundSize}
-          fill={logoBackgroundColor}
-          clipPath='url(#clip-logo-background)'
-        />
-      </G>
-      <G x={logoMargin} y={logoMargin}>
-        <Image
-          width={logoSize}
-          height={logoSize}
-          preserveAspectRatio='xMidYMid slice'
-          href={logo}
-          clipPath='url(#clip-logo)'
-        />
-      </G>
-    </G>
-  )
-}
-
 const QRCode = ({
   value = 'this is a QR code',
   size = 100,
@@ -104,38 +50,7 @@ const QRCode = ({
   const { path, cellSize } = result
 
   return (
-    <Svg
-      ref={getRef}
-      viewBox={[
-        -quietZone,
-        -quietZone,
-        size + quietZone * 2,
-        size + quietZone * 2
-      ].join(' ')}
-      width={size}
-      height={size}
-    >
-      <Defs>
-        <LinearGradient
-          id='grad'
-          x1={gradientDirection[0]}
-          y1={gradientDirection[1]}
-          x2={gradientDirection[2]}
-          y2={gradientDirection[3]}
-        >
-          <Stop offset='0' stopColor={linearGradient[0]} stopOpacity='1' />
-          <Stop offset='1' stopColor={linearGradient[1]} stopOpacity='1' />
-        </LinearGradient>
-      </Defs>
-      <G>
-        <Rect
-          x={-quietZone}
-          y={-quietZone}
-          width={size + quietZone * 2}
-          height={size + quietZone * 2}
-          fill={backgroundColor}
-        />
-      </G>
+    <G height={size}>
       <G>
         <Path
           d={path}
@@ -144,16 +59,7 @@ const QRCode = ({
           strokeWidth={cellSize}
         />
       </G>
-      {logo &&
-        renderLogo({
-          size,
-          logo,
-          logoSize,
-          logoBackgroundColor,
-          logoMargin,
-          logoBorderRadius
-        })}
-    </Svg>
+    </G>
   )
 }
 
